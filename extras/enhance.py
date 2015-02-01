cc11ba056bc86161de88aa4b2c23d68de34665d7
Jerome Leclanche <jerome@leclan.ch>
2015-02-01 14:24:41 +0000

    Add support for guessing Spell Power

--- extras/enhance.py
+++ extras/enhance.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
+import re
 import sys; sys.path.append("../..")
 from xml.etree import ElementTree
 
 from fireplace.cards import cardxml
+from fireplace.enums import GameTag
 import chooseone
 
 
@@ -14,11 +16,24 @@ def add_chooseone_tags(card, ids):
 		card.xml.append(e)
 	print("%s: Adding Choose One cards: %r" % (card.name, ids))
 
+
+def guess_spellpower(card):
+	sre = re.search(r"Spell Damage \+(\d+)", card.description)
+	dmg = int(sre.groups()[0])
+	e = card._findTag(GameTag.SPELLPOWER)[0]
+	e.attrib["value"] = str(dmg)
+	print("%s: Setting Spell Power to %i" % (card.name, dmg))
+
+
 def main():
 	db = cardxml.load("../TextAsset/enUS.txt")
 	for id, card in db.items():
 		if hasattr(chooseone, id):
 			add_chooseone_tags(card, getattr(chooseone, id))
+
+		if card.tags.get(GameTag.SPELLPOWER):
+			guess_spellpower(card)
+
 
 
 if __name__ == "__main__":
