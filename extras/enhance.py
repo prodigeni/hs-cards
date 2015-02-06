489c27a7374456724113c65b1b5b0b9d9e2b38b0
Jerome Leclanche <jerome@leclan.ch>
2015-02-06 08:22:18 +0000

    Use fireplace.cards.cardxml module for parsing xml

--- extras/enhance.py
+++ extras/enhance.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
+import sys; sys.path.append("../..")
 from xml.etree import ElementTree
 
+from fireplace.cards import cardxml
 import chooseone
 
 
@@ -13,14 +15,10 @@ def add_chooseone_tags(card, ids):
 	print("%s: Adding Choose One cards: %r" % (card.name, ids))
 
 def main():
-	path = "../TextAsset/enUS.txt"
-	with open(path, "r") as f:
-		xml = ElementTree.parse(f)
-		for card in xml.findall("Entity"):
-			id = card.attrib["CardID"]
-
-			if hasattr(chooseone, id):
-				add_chooseone_tags(card, getattr(chooseone, id))
+	db = cardxml.load("../TextAsset/enUS.txt")
+	for id, card in db.items():
+		if hasattr(chooseone, id):
+			add_chooseone_tags(card, getattr(chooseone, id))
 
 
 if __name__ == "__main__":
