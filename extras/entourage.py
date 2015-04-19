b4f4e138ae78abdb58b75b375da8883fff234c4e
Jerome Leclanche <jerome@leclan.ch>
2015-04-19 22:41:44 +0000

    Get rid of Bane of Doom entourage

--- extras/entourage.py
+++ extras/entourage.py
@@ -4,8 +4,5 @@ Patch 8311 replaced some cardIDs by useless UUIDs...
 """
 
 
-# Bane of Doom
-EX1_320 = ["EX1_306", "CS2_065", "EX1_319", "EX1_301", "CS2_059", "CS2_064"]
-
 # Ysera
 EX1_572 = ["DREAM_01", "DREAM_02", "DREAM_03", "DREAM_04", "DREAM_05"]
